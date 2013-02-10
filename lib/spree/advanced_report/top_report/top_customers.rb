8fc43930adf1341748238bc1cbd77d2b2f231748
Greg Reinacker <gregr@rassoc.com>
2013-02-10 16:44:35 -0700

    use limit instead of hard-coded 4

--- lib/spree/advanced_report/top_report/top_customers.rb
+++ lib/spree/advanced_report/top_report/top_customers.rb
@@ -23,7 +23,7 @@ class Spree::AdvancedReport::TopReport::TopCustomers < Spree::AdvancedReport::To
     end
 
     self.ruportdata = Table(%w[email Units Revenue])
-    data.inject({}) { |h, (k, v) | h[k] = v[:revenue]; h }.sort { |a, b| a[1] <=> b [1] }.reverse[0..4].each do |k, v|
+    data.inject({}) { |h, (k, v) | h[k] = v[:revenue]; h }.sort { |a, b| a[1] <=> b [1] }.reverse[0..limit].each do |k, v|
       ruportdata << { "email" => data[k][:email], "Units" => data[k][:units], "Revenue" => data[k][:revenue] } 
     end
     ruportdata.replace_column("Revenue") { |r| "$%0.2f" % r.Revenue }
