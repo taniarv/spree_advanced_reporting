3ecd6fc2c7504e491361fb6f528130fabf9d683f
tania <tania@efaber.net>
2021-04-22 18:00:19 +0200

    Include paypal units in total_products report

--- lib/spree/advanced_report/total_report/total_products.rb
+++ lib/spree/advanced_report/total_report/total_products.rb
@@ -51,8 +51,9 @@ class Spree::AdvancedReport::TotalReport::TotalProducts < Spree::AdvancedReport:
           # :paper_units => 0,
           :digital_revenue => 0,
           :digital_units => 0,
+          :paypal_revenue => 0,
+          :paypal_units => 0,
           :revenue => 0,
-          :paypal => 0,
           :units => 0
         }
         if variant.digital?
@@ -67,7 +68,8 @@ class Spree::AdvancedReport::TotalReport::TotalProducts < Spree::AdvancedReport:
         
         # PayPal
         if li.order.payments.last.payment_method_id.eql?(2)
-          data[variant.product_id][:paypal] += li.quantity*li.price
+          data[variant.product_id][:paypal_revenue] += li.quantity*li.price
+          data[variant.product_id][:paypal_units] += li.quantity
         end
         self.total += li.quantity*li.price
         self.total_units += li.quantity
@@ -75,28 +77,31 @@ class Spree::AdvancedReport::TotalReport::TotalProducts < Spree::AdvancedReport:
     end
     
     # self.ruportdata = Table(%w[name paper_units paper_revenue digital_units digital_revenue units revenue])
-    self.ruportdata = Table(%w[name digital_units digital_revenue units paypal revenue])
+    self.ruportdata = Table(%w[name digital_units digital_revenue paypal_units paypal_revenue units revenue])
 
     # data.inject({}) { |h, (k, v) | h[k] = v[:units]; h }.sort { |a, b| a[1] <=> b [1] }.reverse.each do |k, v|
     #   ruportdata << { "name" => data[k][:name], "paper_units" => data[k][:paper_units], "paper_revenue" => data[k][:paper_revenue], 
     #     "digital_units" => data[k][:digital_units], "digital_revenue" => data[k][:digital_revenue], "units" => data[k][:units], "revenue" => data[k][:revenue] } 
     # end
     data.inject({}) { |h, (k, v) | h[k] = v[:units]; h }.sort { |a, b| a[1] <=> b [1] }.reverse.each do |k, v|
-      ruportdata << { "name" => data[k][:name], "digital_units" => data[k][:digital_units], 
-        "digital_revenue" => data[k][:digital_revenue], "units" => data[k][:units], "paypal" => data[k][:paypal], "revenue" => data[k][:revenue] } 
+      ruportdata << { "name" => data[k][:name], 
+        "digital_units" => data[k][:digital_units], "digital_revenue" => data[k][:digital_revenue], 
+        "paypal_units" => data[k][:paypal_units], "paypal_revenue" => data[k][:paypal_revenue], 
+        "units" => data[k][:units], "revenue" => data[k][:revenue] } 
     end
     # ruportdata.replace_column("paper_revenue") { |r| Spree::Money.new(r.paper_revenue).to_s }
     ruportdata.replace_column("digital_revenue") { |r| Spree::Money.new(r.digital_revenue).to_s }
+    ruportdata.replace_column("paypal_revenue") { |r| Spree::Money.new(r.paypal_revenue).to_s }
     ruportdata.replace_column("revenue") { |r| Spree::Money.new(r.revenue).to_s }
-    ruportdata.replace_column("paypal") { |r| Spree::Money.new(r.paypal).to_s }
-
+    
     ruportdata.rename_column("name", Spree.t('adv_report.columns.product_name'))
     # ruportdata.rename_column("paper_units", Spree.t('adv_report.columns.paper_units'))
     # ruportdata.rename_column("paper_revenue", Spree.t('adv_report.columns.paper_revenue'))
     ruportdata.rename_column("digital_units", Spree.t('adv_report.columns.digital_units'))
     ruportdata.rename_column("digital_revenue", Spree.t('adv_report.columns.digital_revenue'))
+    ruportdata.rename_column("paypal_units", Spree.t('adv_report.columns.paypal_units'))
+    ruportdata.rename_column("paypal_revenue", Spree.t('adv_report.columns.paypal_revenue'))
     ruportdata.rename_column("units", Spree.t('adv_report.columns.units'))
-    ruportdata.rename_column("paypal", 'PayPal')
     ruportdata.rename_column("revenue", Spree.t('adv_report.columns.revenue'))
   end
 end
